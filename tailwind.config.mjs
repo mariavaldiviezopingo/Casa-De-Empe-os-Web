ca4d71f143bbc70de404c560798fcb9e2a0756f7
alexispp29 <alexispp290198@gmail.com>
2024-11-26 16:32:00 -0500

    cambioalexis1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,37 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+			  customAzul1:'#0A274A',
+			  customAzul2:'#185DB0',
+			  customDorado:'#D9AA46',
+			  customRojo:'#AC0002'
+			},
+			flexGrow:{
+			  Grow2:'2',
+			  Grow3:'3'
+			},
+			fontSize:{
+			  vwUno:'1vw',
+			  vwDos:'2vw',
+			  vwTres:'3vw',
+			  vwCuatro:'4vw',
+			  vwCinco:'5vw'
+	  
+			},
+			screens:{
+			  sm2: '480px'
+			},
+			fontSize:{
+			  xs2:['0.5rem', { lineHeight: '0.75rem' }]
+			},
+	  
+			gridTemplateColumns:{
+			  autofit1:' repeat(auto-fit,minmax(250px,1fr))'
+			}
+		   
+		  }
 	},
 	plugins: [],
 }
